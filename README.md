52405ccb75fb270c07863c687bb2ff64c0088b2b
bestocacha <46774672+bestocacha@users.noreply.github.com>
2019-05-06 01:18:03 +0700

    Add files via upload
    #6014421010 Nopparut Taechapiroontong

---
+++ README.md
@@ -0,0 +1,1 @@
+#6014421010 Nopparut Taechapiroontong
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    $email = "6014421010@rbru.ac.th";
+    $name = "Nopparut Taechapiroontong";
+    $url = "stu2.rbru.ac.th/~s6014421010";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nopparut</title>
+</head>
+<body>
+<?php
+       echo "<h1>$email</h1>";
+       echo "<h2>$name</h2>";
+       echo "<a href=\"$url\">$name</a>";
+?>
+    
+</body>
+</html>
+
